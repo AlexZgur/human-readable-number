3ef24015d4b01be615ddb5a9ba88c5d8f77145ca
AlexZgur <rugzla@ukr.net>
2020-03-23 21:58:13 +0200

    feat: add toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    let digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let tens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let twentys = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    if (number == 0) return 'zero';
+    let st = '';
+    let c = Math.floor(number / 100);
+    if (c > 0) {
+        st += digits[c - 1] + ' hundred ';
+        number -= c * 100;
+    }
+    if (number > 19) {
+        c = Math.floor(number / 10);
+        st += twentys[c - 2];
+        c = number % 10;
+        if (c > 0) {
+            st += ' ' + digits[c - 1];
+        }
+    } else if (number > 9) {
+        st += tens[number - 10];
+    } else if (number > 0) {
+        st += digits[number - 1];
+    }
+
+    return st.trim();
 }
